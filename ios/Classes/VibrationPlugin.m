11eaecc137a6fd57b87c9c5a051c8615223fe84a
高峰 <gaofeng@xiaoxin.tech>
2019-05-24 16:13:06 +0800

    ios下改用object-c实现

--- ios/Classes/VibrationPlugin.m
+++ ios/Classes/VibrationPlugin.m
@@ -1,8 +1,35 @@
 #import "VibrationPlugin.h"
-#import <vibration/vibration-Swift.h>
+#import <AudioToolbox/AudioToolbox.h>
+//#import <vibration/vibration-Swift.h>
 
-@implementation VibrationPlugin
+@implementation VibrationPlugin {
+    Boolean _isDevice;
+};
+
+- (instancetype)init{
+    self = [super init];
+    _isDevice = TARGET_OS_SIMULATOR == 0;
+    return self;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftVibrationPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"vibration"
+                                     binaryMessenger:[registrar messenger]];
+    VibrationPlugin* instance = [[VibrationPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"hasVibrator" isEqualToString:call.method]) {
+        result([NSNumber numberWithBool:_isDevice]);
+    } else if ([@"vibrate" isEqualToString:call.method]) {
+        AudioServicesPlaySystemSound (kSystemSoundID_Vibrate); 
+        result(nil);
+    } else if([@"cancel" isEqualToString:call.method]) {
+        result(nil);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 @end
